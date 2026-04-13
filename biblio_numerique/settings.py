0899936010460e1d99fa4ba35c4390fbd3568d9b
samy232 <salamanisamy4@gmail.com>
2026-04-13 23:15:36 +0100

    Set DEBUG to False for production readiness

--- biblio_numerique/settings.py
+++ biblio_numerique/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-xr=v*_$5udi^mmzl^w9r19y4_f!!4p_h(92coa7q6#q(17f$g3"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
