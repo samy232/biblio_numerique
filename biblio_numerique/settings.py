fdfcde2158b59a91ee9759c389b619318205e724
samy232 <salamanisamy4@gmail.com>
2026-04-13 23:15:58 +0100

    Update settings.py

--- biblio_numerique/settings.py
+++ biblio_numerique/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-xr=v*_$5udi^mmzl^w9r19y4_f!!4p_h(92coa7q6#q(17f$g3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
