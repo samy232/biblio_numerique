6490a8cbbe29cbad4b43888b12ef017557a3e44a
salamani samy <samy@MacBook-Air-de-salamani.local>
2026-04-14 13:19:25 +0100

    Fix: Autoriser le partage de connexion SQLite (check_same_thread)

--- biblio_numerique/settings.py
+++ biblio_numerique/settings.py
@@ -80,6 +80,10 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+        'OPTIONS': {
+            'timeout': 20,
+            'check_same_thread': False, # <--- AJOUTE CETTE LIGNE
+        },
     }
 }
 
